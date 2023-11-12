985c8af18cbe3835c9669302dd3cf6d28a332339
ELBethan <monpseudojeux@gmail.com>
2023-11-12 16:27:45 +0100

    first real zork on printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,61 @@
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 17:23:18 by eburnet           #+#    #+#             */
-/*   Updated: 2023/11/10 17:35:39 by eburnet          ###   ########.fr       */
+/*   Updated: 2023/11/12 16:27:33 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *, ...)
+#include "./libft/libft.h"
+#include "libftprintf.h"
+
+void	ft_type_management(const char *format, va_list ap)
 {
-	write(1, "Hey", 3);
+	format++;
+	ap++;
+	if (*format == 'c')
+		ft_putchar_fd(va_arg(ap, int), 1);
+	else if (*format == 's')
+		ft_putstr_fd(va_arg(ap, char *), 1);
+	else if (*format == 'd' || *format == 'i')
+		ft_putnbr_fd(va_arg(ap, int), 1);
+	else if (*format == 'x' || *format == 'X')
+		;
+	else if (*format == '%')
+		write(1, "%%", 1);
+	else if (*format == 'p')
+		;
+	else if (*format == 'u')
+		;
+	else
+		return;
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+
+	va_start (ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			ft_type_management(format, ap);
+		}
+		else
+		{
+			while (*format != '\0' && *format != '%')
+			{
+				write(1, format, 1);
+				format++;
+			}
+		}
+		format++;
+	}
+	va_end(ap);
+	return (0);
+}
+\
+int main(void)
+{
+	ft_printf("Salut les %d gars", 5);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,13 +6,15 @@
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:06:52 by eburnet           #+#    #+#             */
-/*   Updated: 2023/11/10 17:28:59 by eburnet          ###   ########.fr       */
+/*   Updated: 2023/11/12 14:37:17 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 # include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-int	ft_printf(const char *, ...)
-#endif /* #ifndef LIBFTPRINTF_H */
+int	ft_printf(const char *format, ...);
+#endif
